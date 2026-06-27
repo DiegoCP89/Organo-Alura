fedc28f5185a3994bdaddc1fa86e1e1305927c26
Diego <diego120689@gmail.com>
2026-06-27 14:38:35 -0300

    Re.Refatorando

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -5,15 +5,6 @@ import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "Devops",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
 
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
@@ -58,11 +49,13 @@ const Formulario = (props) => {
         <ListaSuspensa
           obrigatorio={true}
           label="Time"
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={(valor) => setTime(valor)}
         />
-        <Botao>Criar Card</Botao>
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   );
