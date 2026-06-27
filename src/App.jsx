854ff77e7d3e8671da95cb55139b575350d1b46d
Diego <diego120689@gmail.com>
2026-06-27 10:08:59 -0300

    Salvando no git.

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
+
+function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "",
+      corSecundaria: "#D9F7E9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "",
+      corSecundaria: "#EBF6FF",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "",
+      corSecundaria: "#F0FBE2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "",
+      corSecundaria: " #FAE9F5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "",
+      corSecundaria: "#dfdd9c",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario aoColaboradorCadastrado={(colaborador) => aoNovoColaboradorAdicionado(colaborador)}
+      />
+
+      {times.map((time) => (<Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>))}
+
+      <Time nome="Programação" />
+      <Time nome="Front-End" />
+      <Time nome="Data Science" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img
+        src="/public/imagens/banner.png"
+        alt="Banner principal da página do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
